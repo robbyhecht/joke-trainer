963c467d1854073941506e9c376bc773702f8cc7
robbyhecht <robbyhecht@gmail.com>
2019-03-04 13:42:12 -0600

    data loop for categories in sidebar now persists across site via context_processors.py and adding that to template settings. card flip loops now persist on jokes-by-category page by adding joke.id to the looped div id.

--- jt/static/jt/js/flipcard.js
+++ jt/static/jt/js/flipcard.js
@@ -1,7 +1,4 @@
-// el = element, clr = color
-
 function flipTheCard(card) {
   let flipcard = document.getElementById(card)
   flipcard.classList.toggle('flipit')
-  // elem.style.background = clr
 }
