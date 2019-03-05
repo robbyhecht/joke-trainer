ff60311fcdd622668689f60c2bb3729463255102
robbyhecht <robbyhecht@gmail.com>
2019-03-04 22:28:11 -0600

    added login token to list favorites function. working on login functionality.

--- jt/forms/forms.py
+++ jt/forms/forms.py
@@ -1,12 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
 
-COLOR_CHOICES = (
-    ('red', 'Red'),
-    ('green', 'Green'),
-    ('blue', 'Blue')
-)
-
 class UserForm(forms.ModelForm):
     
     email = forms.EmailField()
@@ -16,8 +10,8 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password', 'first_name', 'last_name',)
 
-class LoginForm(forms.ModelForm):
+# class LoginForm(forms.ModelForm):
 
-    class Meta:
-      model = User
-      fields = ('username', 'password')
+#     class Meta:
+#       model = User
+#       fields = ('username', 'password')
