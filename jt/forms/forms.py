2605f83ef42883d19a63368afa7b37a2035608e3
robbyhecht <robbyhecht@gmail.com>
2019-03-04 20:49:05 -0600

    experimenting with authentication. Register new user is working. Logout redirects to home page and works from the navbar.

--- jt/forms/forms.py
+++ jt/forms/forms.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.models import User
 from django import forms
 
+COLOR_CHOICES = (
+    ('red', 'Red'),
+    ('green', 'Green'),
+    ('blue', 'Blue')
+)
+
 class UserForm(forms.ModelForm):
+    
+    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'first_name', 'last_name',)
+
+class LoginForm(forms.ModelForm):
+
+    class Meta:
+      model = User
+      fields = ('username', 'password')
--- jt/views/views.py
+++ jt/views/views.py
@@ -29,14 +29,10 @@ def register(request):
     # on Django's built-in User model
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
-        # customer_form = CustomerForm(data=request.POST)
 
-        if user_form.is_valid() and customer_form.is_valid():
+        if user_form.is_valid():
             # Save the user's form data to the database.
             user = user_form.save()
-            # customer = customer_form.save(commit=False)
-            # customer.user = user
-            # customer.save()
 
             # Now we hash the password with the set_password method.
             # Once hashed, we can update the user object.
@@ -45,19 +41,14 @@ def register(request):
 
             # Update our variable to tell the template registration was successful.
             registered = True
-            # payment = PaymentType.objects.create(name= "southwest", accountNumber = "12355346", customer= customer)
             user_id = user.id
-            # Order.objects.create(isCompleted = 0, customer= customer, paymentType = payment)
 
         return login_user(request)
 
     elif request.method == 'GET':
         user_form = UserForm()
-        # customer_form = CustomerForm()
         template_name = 'register.html'
-        return render(request, template_name, {'user_form': user_form,
-        # 'customer_form': customer_form
-        })
+        return render(request, template_name, {'user_form': user_form,})
 
 
 def login_user(request):
