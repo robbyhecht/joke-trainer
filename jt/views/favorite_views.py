563141e21b81e74de1b15918faf7baf54cdba9f7
robbyhecht <robbyhecht@gmail.com>
2019-03-06 14:06:06 -0600

    cleaned up code

--- jt/views/favorite_views.py
+++ jt/views/favorite_views.py
@@ -17,22 +17,3 @@ def favorites_list(request, id):
   context = { 'favorite_jokes' : favorite_jokes, 'joke_detail' : joke_detail }
   print("faves", context)
   return render(request, 'favorite_jokes.html', context)
-
-
-def list_categories(request):
-  '''Handles listing joke categories in sidebar'''
-  # retrieves all categories
-  category_list = Category.objects.all()
-  # creates a dictionary to pass departments and employees to the HTML template
-  context ={'category_list' : category_list}
-  return render(request, 'index.html', context)
-
-def list_by_category(request, id):
-  '''Handles listing jokes by category...
-  Category name is accessed in joke_category
-  joke_content filters using join table to match up jokes with categories'''
-  joke_category = get_object_or_404(Category, pk= id)
-  joke_content = Joke.objects.filter(category = id)
-  context = { 'joke_category' : joke_category, 'joke_content' : joke_content }
-  print(context)
-  return render(request, 'joke_category.html', context)
