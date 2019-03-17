f760dffe42b1bb57447e3db0b14d328faf8c6fdf
robbyhecht <robbyhecht@gmail.com>
2019-03-17 12:24:27 -0500

    on myjokes page, delete button is now correctly bring up deletion modal and modal delete button is functional. cancal and x buttons not yet functional. still need to add this functionality to the search and category pages.

--- jt/static/jt/js/flipcard.js
+++ jt/static/jt/js/flipcard.js
@@ -2,3 +2,8 @@ function flipTheCard(card) {
   let flipcard = document.getElementById(card)
   flipcard.classList.toggle('flipit')
 }
+
+function deletionModal(modal) {
+  let deletion = document.getElementById(modal)
+  deletion.classList.toggle('is-active')
+}
