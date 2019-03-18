f2f33d952e3d1b4431bd6fc8c6e4f49c681bb34e
robbyhecht <robbyhecht@gmail.com>
2019-03-17 20:19:30 -0500

    cleaned up code and added comments to js functions

--- jt/static/jt/js/main.js
+++ jt/static/jt/js/main.js
@@ -1,13 +1,18 @@
+// EVENT HANDLERS
+
+// handles flashcard flipping
 function flipTheCard(card) {
   let flipcard = document.getElementById(card)
   flipcard.classList.toggle('flipit')
 }
 
+// handles opening delete modal
 function deletionModal(modal) {
   let deletion = document.getElementById(modal)
   deletion.classList.add('is-active')
 }
 
+// handles closing delete modal
 function closeDeletionModal(modal) {
   let closeDeletion = document.getElementById(modal)
   closeDeletion.classList.remove('is-active')
