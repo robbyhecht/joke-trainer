f5204c6bb890a0b1ed0b06ca73256ed0dc6ba6ce
robbyhecht <robbyhecht@gmail.com>
2019-07-12 16:01:47 -0500

    added an invalid login form retry template to replace the simple invalid login message

--- jt/views/views.py
+++ jt/views/views.py
@@ -102,7 +102,12 @@ def login_user(request):
       else:
           # Bad login details were provided. So we can't log the user in.
           print("Invalid login details: {}, {}".format(username, password))
-          return HttpResponse("Invalid login details supplied.")
+          # return HttpResponse("Invalid login details supplied.")
+          login_form = LoginForm()
+          context = {'next': request.GET.get('next', '/')
+  , 'login_form': login_form,}
+          template_name = 'login_rejected.html'
+          return render(request, template_name, context)
 
     elif request.method == 'GET':
       login_form = LoginForm()
